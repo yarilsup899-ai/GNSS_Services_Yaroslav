befce7f22aeaa5966df121eb0718bf175bac25d5
yarilsup899-ai <yaril.sup899@gmail.com>
2025-10-30 07:35:39 +0300

    Add files via upload

--- clientfile.py
+++ clientfile.py
@@ -17,40 +17,40 @@ def recv_exc(socket, n):
  # Функция отправки   
 def send_rinex(host: str, port: int, filepath: str):
     if not os.path.isfile(filepath):
-    print(f"Ошибка, файла нет - {filepath}")
-    return
+        print(f"Ошибка, файла нет - {filepath}")
+        return
 
-with open(filepath, "rb") as f:
-    file_data = f.read()
-filename = os.path.basename(filepath)
+    with open(filepath, "rb") as f:
+        file_data = f.read()
+    filename = os.path.basename(filepath)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as SERVER:
-    SERVER.connect((host, port))
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as SERVER:
+        SERVER.connect((host, port))
 
-#Отправка файлов
-SERVER.sendall(struct.pack('>I', len(filename)))
-SERVER.sendall(filename.encode('utf-8'))
+    #Отправка файлов
+    SERVER.sendall(struct.pack('>I', len(filename)))
+    SERVER.sendall(filename.encode('utf-8'))
 
-SERVER.sendall(struct.pack('>Q', len(file_data)))
-SERVER.sendall(file_data)
+    SERVER.sendall(struct.pack('>Q', len(file_data)))
+    SERVER.sendall(file_data)
 
-#Прием ответа
-prefix = recv_exc(SERVER, 4)
+    #Прием ответа
+    prefix = recv_exc(SERVER, 4)
 
-if prefix == b"OK::":
-    size_bytes = recv_exc(SERVER, 8)
-    result_size = struct.unpack('>Q', size_bytes)[0]
+    if prefix == b"OK::":
+        size_bytes = recv_exc(SERVER, 8)
+        result_size = struct.unpack('>Q', size_bytes)[0]
 
-    result = recv_exc(SERVER, result_size)
-    print("\n===Результат обработки")
-    print(result.decode('utf-8'))
+        result = recv_exc(SERVER, result_size)
+        print("\n===Результат обработки")
+        print(result.decode('utf-8'))
 
-elif prefix.startswith(b"ERR"):
-    rest = SERVER.recv(1024)
-    full_error = (prefix + rest).decode('utf-8', errors='repalce')
-    print("Сервер вернул ошибку:", full_error)
-else
-    print("Некорректный ответ от сервера:", repr(prefix))
+    elif prefix.startswith(b"ERR"):
+        rest = SERVER.recv(1024)
+        full_error = (prefix + rest).decode('utf-8', errors='repalce')
+        print("Сервер вернул ошибку:", full_error)
+    else:
+        print("Некорректный ответ от сервера:", repr(prefix))
 
 if __name__ == '__main__':
     if len(sys.argv) !=2:
